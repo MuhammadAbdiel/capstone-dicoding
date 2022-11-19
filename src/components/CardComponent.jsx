e8ae7aeafdb66c8e5dd35dcdcda860c42e3976c0
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-11-19 14:39:13 +0700

    add card component

---
+++ src/components/CardComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import logo from "../images/logo192.png";
+
+function CardComponent() {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={logo} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button style={{ backgroundColor: "#2155CD" }}>Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default CardComponent;
