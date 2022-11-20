a068ff5e7654b94f454bcb80648f99fa6b39ed01
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-11-20 20:21:06 +0700

    update authorize function from true into Gate::allows in StoreArticleRequest and UpdateArticleRequest

--- app/Http/Requests/StoreArticleRequest.php
+++ app/Http/Requests/StoreArticleRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 
 class StoreArticleRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class StoreArticleRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return Gate::allows('create-article');
     }
 
     /**
--- app/Http/Requests/UpdateArticleRequest.php
+++ app/Http/Requests/UpdateArticleRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 
 class UpdateArticleRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class UpdateArticleRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return Gate::allows('update-article');
     }
 
     /**
