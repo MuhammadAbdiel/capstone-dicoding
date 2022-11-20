64c04d53fdf68075ea8b73b89353fc0b44ddb361
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2022-11-20 17:26:38 +0700

    Update NotFoundPage, add react-icons package

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import HeaderComponent from '../components/HeaderComponent'
+import { TiWarningOutline } from 'react-icons/ti'
 
 const NotFoundPage = () => {
   return (
     <>
       <HeaderComponent />
-      <div className='d-sm-flex flex-column mt-3 not-found'>
-        <h1>404</h1>
-        <p>Page Not Found!</p>
-        <p>Halaman yang anda minta tidak dapat ditemukan!</p>
+      <div className='d-flex justify-content-center'>
+        <div className='d-flex flex-column align-items-center mt-3 pb-3 rounded-3 not-found'>
+          <TiWarningOutline className='warning-icon' />
+          <h1>404</h1>
+          <p>Page Not Found!</p>
+          <p>Halaman yang anda minta tidak dapat ditemukan!</p>
+        </div>
       </div>
     </>
   )
