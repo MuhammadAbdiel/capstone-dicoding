1461c75ff782955794c475ff9a7c690ab7c95651
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2022-11-21 08:19:04 +0700

    memperbarui component footer style

--- src/components/FooterStyleComponent.jsx
+++ src/components/FooterStyleComponent.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import { FaInstagram } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
 
 const FooterStyleComponent = () => {
   return (
     <footer className='card text-center position-sticky w-100 mt-4'>
-      <div className='card-header' style={{ backgroundColor: '#0AA1DD' }}>
-        Featured
-      </div>
       <div className='card-body'>
         <h5 className='card-title'>Special title treatment</h5>
         <p className='card-text'>With supporting text below as a natural lead-in to additional content.</p>
-        <a href='#' className='btn' style={{ backgroundColor: '#0AA1DD' }}>
-          Go somewhere
+        <a href='#' className='btn mx-1' style={{ backgroundColor: '#0AA1DD' }}>
+          <FaInstagram />
+        </a>
+        <a href='#' className='btn mx-1' style={{ backgroundColor: '#0AA1DD' }}>
+          <FaLinkedin />
+        </a>
+        <a href='#' className='btn mx-1' style={{ backgroundColor: '#0AA1DD' }}>
+          <FaInstagram />
         </a>
       </div>
-      <div className='card-footer' style={{ backgroundColor: '#0AA1DD' }}>
+      <div className='footer p-2' style={{ backgroundColor: '#0AA1DD' }}>
         Tiketin Apps
       </div>
     </footer>
