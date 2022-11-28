1aeb13d2b65f045e47197b26cb0ea105bd50b24a
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-28 10:18:33 +0700

    add descrp text-overlow

--- src/components/CardArticlesComponent.jsx
+++ src/components/CardArticlesComponent.jsx
@@ -7,9 +7,13 @@ const CardArticlesComponent = () => {
     <Card className='m-4'>
       <Card.Body>
         <Card.Title>Article title</Card.Title>
-        <Card.Text>
+        <Card.Text className='descrp'>
           Lorem Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur laboriosam atque nostrum nihil ullam dicta! Quia modi,
-          expedita maiores, quam alias omnis blanditiis cupiditate magni libero accusantium sapiente, eligendi ducimus.
+          expedita maiores, quam alias omnis blanditiis cupiditate magni libero accusantium sapiente, eligendi ducimus. expedita maiores,
+          quam alias omnis blanditiis cupiditate magni libero accusantium sapiente, eligendi ducimus. Lorem Lorem ipsum dolor sit, amet
+          consectetur adipisicing elit. Consequatur laboriosam atque nostrum nihil ullam dicta! Quia modi, expedita maiores, quam alias
+          omnis blanditiis cupiditate magni libero accusantium sapiente, eligendi ducimus. expedita maiores, quam alias omnis blanditiis
+          cupiditate magni libero accusantium sapiente, eligendi ducimus.
         </Card.Text>
         <div className='d-flex justify-content-end'>
           <Button variant='primary'>View More</Button>
--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -9,7 +9,9 @@ const CardComponent = () => {
       <Card.Img variant='top' src={logo} />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
-        <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+        <Card.Text className='descrp'>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </Card.Text>
         <Button style={{ backgroundColor: '#2155CD' }}>Go somewhere</Button>
       </Card.Body>
     </Card>
