98cecda14f478d42a79cf6a495d174a88df3eebf
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2022-12-01 05:34:22 +0700

    Update Login.jsx
    - Add email and password validation
    - Prevents calling requests API when there is no input
    - Add loading indicator while waiting response from login API
    
    Needs to be fixed :
    - API response error should be true if user credentials is invalid

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import HeaderComponent from '../components/HeaderComponent'
@@ -6,44 +6,107 @@ import { Link, useNavigate } from 'react-router-dom'
 import FooterStyleComponent from '../components/FooterStyleComponent'
 import useInput from '../components/useInput'
 import { login, putAccessToken } from '../utils/network-data'
+import { IoMdCloseCircle } from 'react-icons/io'
+import { alertIfFoundMissingInput } from '../utils/alertMissingInputForm'
+import LoadingIndicatorComponent from '../components/LoadingIndicatorComponent'
 // import FooterComponent from '../components/FooterComponent'
 
 const Login = () => {
   const navigate = useNavigate()
   const [email, handleEmailChange] = useInput('')
   const [password, handlePasswordChange] = useInput('')
+  const [isEmailValid, setIsEmailValid] = useState('Not Set')
+  const [isPasswordValid, setIsPasswordValid] = useState('Not Set')
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
+    if (email !== '') {
+      if (/\S+@\S+\.\S+/.test(email)) {
+        setIsEmailValid(true)
+      } else {
+        setIsEmailValid(false)
+      }
+    } else if (email === '') {
+      setIsEmailValid('Not Set')
+    }
+  }, [email])
+
+  useEffect(() => {
+    if (password !== '') {
+      if (password.length >= 8) {
+        setIsPasswordValid(true)
+      } else {
+        setIsPasswordValid(false)
+      }
+    } else if (password === '') {
+      setIsPasswordValid('Not Set')
+    }
+  }, [password])
 
   const onSubmitHandler = async (event) => {
     event.preventDefault()
-    const response = await login({ email, password })
-    console.log(response)
-    try {
-      if (!response.error) {
-        putAccessToken(response.data.access_token)
-        navigate('/')
+    if (email !== '' && password !== '' && isEmailValid && isPasswordValid) {
+      setIsLoading(true)
+      const response = await login({ email, password })
+      console.log(response)
+      try {
+        if (!response.error) {
+          setIsLoading(false)
+          putAccessToken(response.data.access_token)
+          navigate('/')
+        }
+      } catch (error) {
+        setIsLoading(false)
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: error
+        })
       }
-    } catch (error) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: error
-      })
+    } else if (email === '' || password === '' || !isPasswordValid) {
+      alertIfFoundMissingInput({ email, password, isPasswordValid })
     }
   }
 
   return (
     <>
+      {isLoading && <LoadingIndicatorComponent />}
       <HeaderComponent />
       <div className=' d-flex justify-content-center my-5 '>
         <Form style={{ width: '80%' }} onSubmit={onSubmitHandler}>
-          <Form.Group className='mb-3' controlId='formEmail'>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter your email' value={email} onChange={handleEmailChange} />
+          <Form.Group
+            className='mb-3'
+            controlId='formEmail'
+            title={isEmailValid === false ? 'Please enter valid email address' : undefined}
+          >
+            <Form.Label>
+              Email address
+              {isEmailValid === false && <IoMdCloseCircle color='red' />}
+            </Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter your email'
+              value={email}
+              onChange={handleEmailChange}
+              className={isEmailValid === false && 'invalid'}
+            />
           </Form.Group>
 
-          <Form.Group className='mb-3 ' controlId='formPassword'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type='password' placeholder='Enter your Password' value={password} onChange={handlePasswordChange} />
+          <Form.Group
+            className='mb-3'
+            controlId='formPassword'
+            title={isPasswordValid === false ? 'Your password must containt at least 8 characters' : undefined}
+          >
+            <Form.Label>
+              Password
+              {isPasswordValid === false && <IoMdCloseCircle color='red' />}
+            </Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter your Password'
+              value={password}
+              onChange={handlePasswordChange}
+              className={isPasswordValid === false && 'invalid'}
+            />
           </Form.Group>
 
           <Button style={{ width: '100%', backgroundColor: '#0AA1DD' }} className='mb-3 fw-bold' variant='primary' type='submit'>
