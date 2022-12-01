f467d5fcb698e83005467d1986ab0a80d611d25d
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-01 21:05:39 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,9 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Travel from '../images/assets/Journey-amico.png'
+import Article from '../images/assets/article-news.jpg'
 import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -24,15 +26,13 @@ const Home = () => {
           <button>Start My Trip</button>
         </div>
       </div>
-      <div className='mt-5'>
+      <div className='mt-5 '>
         <Container>
-          <Row>
+          <Row className='d-flex justify-content-evenly'>
             <Col sm={5}>
-              <div>
-                <Card.Img height={400} width={800} src={Travel} />
-              </div>
+              <Card.Img height={400} width={800} src={Travel} />
             </Col>
-            <Col sm={5} className='mt-5'>
+            <Col sm={5} className='mt-5 p-lg-5'>
               <h2 className='text-center'>Want to Go on Vacation?</h2>
               <p>
                 Have you ever been confused to find information about interesting tours in Malang? or looking for places that are popular
@@ -48,9 +48,31 @@ const Home = () => {
           </Row>
         </Container>
       </div>
+      <div>
+        <Container>
+          <Row className='d-flex justify-content-evenly'>
+            <Col sm={5} className='mt-5'>
+              <div>
+                <Card.Img height={500} width={800} src={Article} />
+              </div>
+            </Col>
+            <Col sm={5} className='mt-5 p-lg-5'>
+              <h2 className='text-center'>Latest news about travel</h2>
+              <p>
+                Di NAMA juga tersedia halaman artickel yang mempermudah anda untuk mencari tahu berita-berita terupdate seputar pariwisata
+                yang ada di sekitar kabupaten semarang.
+              </p>
+              <div className='d-flex justify-content-end'>
+                <Link to='/articles'>
+                  <Button variant='primary'>View</Button>
+                </Link>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
       <div>onProgress</div>
-      <div>onProgress</div>
-      {/* <h1 className='text-center fs-3 mt-3'>Lorem</h1>
+      <h1 className='text-center fs-3 mt-3'>Lorem</h1>
       <div className='d-grid container mb-4'>
         <div className='row'>
           <p className='col'>
@@ -66,7 +88,7 @@ const Home = () => {
             dolorem quisquam, esse fuga modi in sint accusantium incidunt ratione ullam possimus eos neque quibusdam.
           </p>
         </div>
-      </div> */}
+      </div>
       <FooterComponent />
     </>
   )
