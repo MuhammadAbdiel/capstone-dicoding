9eff8183d092bc87f5a3400f8197cb80ff93039a
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2022-12-03 14:33:54 +0700

    Update Explore
    - Add image
    - Add margin

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -5,12 +5,12 @@ import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
 import { Link } from 'react-router-dom'
 
-const CardComponent = ({ name, description, id }) => {
+const CardComponent = ({ name, description, id, image }) => {
   return (
-    <Col className='my-5' lg={3} md={4} sm={6}>
-      <Card>
-        {/* <Card.Img variant='top' src={destination_galleries} /> */}
-        <Card.Body>
+    <Col className='my-2' lg={3} md={4} sm={6}>
+      <Card className='h-100'>
+        <Card.Img variant='top' src={image} className='card-img' />
+        <Card.Body className='d-flex flex-column justify-content-between'>
           <Card.Title>{name}</Card.Title>
           <Card.Text className='descrp'>{description}</Card.Text>
           <Link to={`/tourism/${id}`}>
