91ba65ea9ad6e36099fe2c22510e1c3cec8c06ee
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2022-12-04 14:43:41 +0700

    fix seeder

--- database/seeders/ArticleGallerySeeder.php
+++ database/seeders/ArticleGallerySeeder.php
@@ -18,10 +18,20 @@ class ArticleGallerySeeder extends Seeder
             [
                 'article_id' => 1,
                 'image' => 'https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1234/https://hawaiwaterpark.com/wp-content/uploads/2021/08/peraturan-di-hawai-waterpark.jpeg',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
                 'article_id' => 2,
                 'image' => 'https://jtp.id/images/news/20220830092151_Screenshot_833.png',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
                 'article_id' => 3,
                 'image' => 'https://malangnightparadise.com/wp-content/uploads/2021/09/taman-dinosaurus.jpeg',
+                'created_at' => now(),
+                'updated_at' => now()
             ]
         ];
 
