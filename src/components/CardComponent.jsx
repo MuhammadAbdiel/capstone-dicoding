f53dc9c011bdcca90e97075ed86d14a2889b35ee
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-07 07:31:07 +0700

    add animation in card commponet toursim zoom-in

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 // import logo from '../images/logo192.png'
 import Col from 'react-bootstrap/Col'
 import { Link } from 'react-router-dom'
+import AOS from 'aos'
 
 const CardComponent = ({ name, description, id, image }) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000
+    })
+  }, [])
   return (
     <Col className='my-2' lg={3} md={4} sm={6}>
-      <Card className='h-100'>
+      <Card className='h-100' data-aos='zoom-in'>
         <Card.Img variant='top' loading='lazy' src={image} className='img-card-tourism' />
         <Card.Body className='d-flex flex-column justify-content-between'>
           <Card.Title>{name}</Card.Title>
