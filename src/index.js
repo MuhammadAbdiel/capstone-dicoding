08145be7ba6710a6bb15e660d17a07d089e26bb7
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-07 07:15:56 +0700

    add aos animation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'aos/dist/aos.css'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
